38b117c76c1173a863ac8da24d107a36dbdc3c98
admin@example.com <admin@exmaple.com>
2025-06-04 10:27:13 -0500

    Tested out cosine similarity

--- backend_lambda/main.py
+++ backend_lambda/main.py
@@ -1,12 +1,18 @@
 from openai import OpenAI
-# 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
-#embedding = openai.Embedding.create(input="Are you authorized to work in the US?", model="text-embedding-3-small")["data"][0]["embedding"]
+from scipy.spatial.distance import cosine
+
 client = OpenAI(api_key="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")
 
-response=client.embeddings.create(
+vector1=client.embeddings.create(
   model="text-embedding-ada-002",
-  input="The food was delicious and the waiter...",
+  input="What is your Grade Point Average?",
   encoding_format="float"
-)
+).data[0].embedding
 
-print(response)
+vector2=client.embeddings.create(
+  model="text-embedding-ada-002",
+  input="GPA",
+  encoding_format="float"
+).data[0].embedding
+
+print(1-cosine(vector1, vector2))
